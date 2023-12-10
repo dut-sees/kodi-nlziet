af72dea69e378641f00d64bc2a811fa57d051881
dut-sees <dut-sees@proton.me>
2023-12-10 15:13:23 +0100

    chore(nlziet): most viewed on top

--- plugin.video.nlziet/resources/lib/constants.py
+++ plugin.video.nlziet/resources/lib/constants.py
@@ -89,6 +89,7 @@ CONST_MOD_CACHE = {}
 CONST_VOD_CAPABILITY = [
     #{ 'file': 'movies', 'label': _.MOVIES, 'start': 0, 'menu': 0, 'online': 0, 'search': 1, 'az': 1 },
     #{ 'file': 'series', 'label': _.SERIESBINGE, 'start': 0, 'menu': 0, 'online': 0, 'search': 1, 'az': 1 },
+    { 'file': 'mostviewed', 'label': _.MOSTVIEWED, 'start': 0, 'menu': 0, 'online': 1, 'search': 1, 'az': 0 },
     { 'file': 'tipfeednpo', 'label': _.RECOMMENDED + ' - NPO Plus', 'start': 0, 'menu': 0, 'online': 1, 'search': 1, 'az': 0 },
     { 'file': 'tipfeed', 'label': _.RECOMMENDED, 'start': 0, 'menu': 0, 'online': 1, 'search': 1, 'az': 0 },
     { 'file': 'watchaheadnpo', 'label': _.WATCHAHEAD + ' - NPO Plus', 'start': 0, 'menu': 0, 'online': 1, 'search': 1, 'az': 0 },
@@ -97,7 +98,6 @@ CONST_VOD_CAPABILITY = [
     { 'file': 'movies', 'label': _.MOVIES, 'start': 0, 'menu': 0, 'online': 1, 'search': 1, 'az': 0 },
     { 'file': 'seriesbingenpo', 'label': _.SERIESBINGE + ' - NPO Plus', 'start': 0, 'menu': 0, 'online': 1, 'search': 1, 'az': 0 },
     { 'file': 'seriesbinge', 'label': _.SERIESBINGE, 'start': 0, 'menu': 0, 'online': 1, 'search': 1, 'az': 0 },
-    { 'file': 'mostviewed', 'label': _.MOSTVIEWED, 'start': 0, 'menu': 0, 'online': 1, 'search': 1, 'az': 0 },
 ]
 
 #"show"
